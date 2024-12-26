5103cd80172c0c44c76d65b18108d1c4c2e8681d
Shivankar Patra <146526518+shivankarpatra04@users.noreply.github.com>
2024-12-26 19:24:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
         ? process.env.FRONTEND_URL
-        : ['https://portfolio-website-frontend-gray.vercel.app/'], // Replace with your frontend URL during development
+        : ['https://portfolio-website-frontend-gilt.vercel.app/'], // Replace with your frontend URL during development
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
